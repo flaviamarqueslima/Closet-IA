a905fcf7b5579499d48e572f5a7044c5a39f66b2
flaviamarqueslima <flmarlim@gmail.com>
2026-08-31 16:51:19 -0300

    Fix syntax error in SUPABASE_URL declaration

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = https://qnntmkvdqlpingedabml.supabase.co";
+const SUPABASE_URL ="https://qnntmkvdqlpingedabml.supabase.co";
 
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFubnRta3ZkcWxwaW5nZWRhYm1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgyMDE0NTIsImV4cCI6MjEwMzc3NzQ1Mn0.pP2zaPFxPt_i9R99RC0HYVMRdwvVhZrqi3SmsOEWv8A";
 
