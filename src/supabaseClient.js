cb2d56c0d0537c96039064616266588b15c08ee4
flaviamarqueslima <flmarlim@gmail.com>
2026-08-31 16:46:45 -0300

    Update Supabase client URL and anon key
    Updated Supabase client configuration with actual URL and anon key.

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "SUA_URL_AQUI";
-const SUPABASE_ANON_KEY = "SUA_ANON_KEY_AQUI";
+const SUPABASE_URL = https://qnntmkvdqlpingedabml.supabase.co";
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFubnRta3ZkcWxwaW5nZWRhYm1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgyMDE0NTIsImV4cCI6MjEwMzc3NzQ1Mn0.pP2zaPFxPt_i9R99RC0HYVMRdwvVhZrqi3SmsOEWv8A";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
